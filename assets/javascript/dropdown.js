9d15b73b1684754f2c0ef6be34f88871e1fa18f9
Marko <marko.milky@gmail.com>
2018-03-19 20:13:10 +0100

    refactoring

---
+++ assets/javascript/dropdown.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("dropdown-toggle").click(function(event) {
+    $("dropdown").removeClass('active');
+    $(this).parent("dropdown").addClass('active');
+    event.stopPropagation();
+  });
+  $(window, "dropdown-item").click(function() {
+    $("dropdown").removeClass('active');
+  });
+});
---
+++ assets/javascript/modal.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $("[opens]").click(function() {
+    thisOpens = $(this).attr("opens");
+    $('#' + thisOpens).addClass('active');
+  });
+  $("modal-overlay, [closeModal]").click(function(){
+    $("modal").removeClass("active");
+  });
+});
