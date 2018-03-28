6526be0e4260a83469ab1ea1cfba9f9b4cec870d
Marko <marko.milky@gmail.com>
2018-03-28 20:03:10 +0200

    Updated modal

--- assets/javascript/modal.js
+++ assets/javascript/modal.js
@@ -12,6 +12,10 @@ window.onload = () => {
         const modalEl = document.getElementById(el.getAttribute('opens'));
         el.addEventListener('click', () => {
             modalEl.classList.add('active');
+            const modalHeaderEl = modalEl.querySelector('modal-header').scrollHeight;
+            const modalFooterEl = modalEl.querySelector('modal-footer').scrollHeight;
+            const modalBodyEl = modalEl.querySelector('modal-body');
+            modalBodyEl.style.maxHeight = ('calc(100vh - 4rem - ' + (modalHeaderEl + modalFooterEl) + 'px)');
         })
     });
 
