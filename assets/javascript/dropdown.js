f5fc675403c00578fd1cd57238fc08b952b841a3
siclol <marko.milky@gmail.com>
2018-04-02 10:29:07 +0200

    Pure JS for dropdown, minor refactoring

--- assets/javascript/dropdown.js
+++ assets/javascript/dropdown.js
@@ -1,13 +1,28 @@
-$(document).ready(function(){
+/*==================================================
 
-    $("dropdown-toggle").click(function(event) {
-        $("dropdown").removeClass('active');
-        $(this).parent("dropdown").addClass('active');
-        event.stopPropagation();
+  Dropdown
+
+==================================================*/
+
+window.onload = () => {
+
+    const dropdownToggle = document.querySelectorAll('dropdown-toggle');
+    const dropdowns = document.querySelectorAll('dropdown');
+
+    dropdownToggle.forEach(el => {
+        el.addEventListener('click', (event) => {
+            dropdowns.forEach(el => {
+                el.classList.remove('active');
+            });
+            el.parentElement.classList.add('active');
+            event.stopPropagation();
+        });
     });
 
-    $(window, "dropdown-item").click(function() {
-        $("dropdown").removeClass('active');
+    window.addEventListener('click', () => {
+        dropdowns.forEach(el => {
+            el.classList.remove('active');
+        });
     });
 
-});
+};
