c7e682a167d7a3a283b58c410091ea863aa4056c
flauc <filip.lauc93@gmail.com>
2018-04-02 18:43:44 +0200

    feat: Making shit happen for real

--- assets/javascript/accordion.js
+++ assets/javascript/accordion.js
@@ -6,36 +6,46 @@
 
 window.onload = () => {
 
-    const accordions = document.querySelectorAll('accordion');
+  const accordions = document.querySelectorAll('accordion');
 
-    const button = document.querySelector('#lul');
+  const button = document.querySelector('#lul');
 
-    accordions.forEach( el => {
-        let toggle = el.childNodes[1];
-        let panel = el.childNodes[3];
+  accordions.forEach( el => {
+    let toggle = el.querySelector('accordion-toggle');
+    let panel = el.querySelector('accordion-panel');
 
-        if (el.hasAttribute("opened")) {
-            panel.style.maxHeight = (panel.scrollHeight + 'px');
-        } else {
-            panel.style.maxHeight = '0';
-        }
+    if (el.hasAttribute("opened")) {
+      panel.style.maxHeight = (panel.scrollHeight + 'px');
+    } else {
+      panel.style.maxHeight = '0';
+    }
 
-        toggle.addEventListener('click', () => {
+    toggle.addEventListener('click', () => {
+      if (el.hasAttribute("opened")) {
 
-            if (el.hasAttribute("opened")) {
-                panel.style.maxHeight = '0';
-                el.removeAttribute('opened');
-            } else {
-                panel.style.maxHeight = (panel.scrollHeight + 'px');
-                el.setAttribute('opened', '');
-            }
+        setAccordionsHeight(panel, true);
+        panel.style.maxHeight = '0';
 
-        });
+        el.removeAttribute('opened');
+      } else {
+        panel.style.maxHeight = (panel.scrollHeight + 'px');
 
-        button.addEventListener('click', () => {
-            console.log(el.clientHeight);
-        });
+        setAccordionsHeight(panel);
+        el.setAttribute('opened', '');
+      }
 
     });
+  });
 
 };
+
+function setAccordionsHeight(panel, remove = false) {
+  let el = panel;
+
+  while (el.parentElement) {
+    if (el.parentElement.nodeName === 'ACCORDION-PANEL') {
+      el.parentElement.style.maxHeight = (parseInt(el.parentElement.style.maxHeight.replace('px', '')) + (remove ? -panel.scrollHeight : panel.scrollHeight)) + 'px';
+    }
+    el = el.parentElement;
+  }
+}
