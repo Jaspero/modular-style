3fe6d18c7db5cda5400b4dbcef20836b07ff4bea
siclol <marko.milky@gmail.com>
2018-04-02 14:00:48 +0200

    Working on accordion

--- assets/javascript/accordion.js
+++ assets/javascript/accordion.js
@@ -1,19 +1,41 @@
-$(document).ready(function(){
+/*==================================================
 
-    $("accordion-panel").each(function(){
-        if ($(this).is("[opened]")) {
-            $(this).css("max-height", $(this).prop("scrollHeight"));
+  Accordion
+
+==================================================*/
+
+window.onload = () => {
+
+    const accordions = document.querySelectorAll('accordion');
+
+    const button = document.querySelector('#lul');
+
+    accordions.forEach( el => {
+        let toggle = el.childNodes[1];
+        let panel = el.childNodes[3];
+
+        if (el.hasAttribute("opened")) {
+            panel.style.maxHeight = (panel.scrollHeight + 'px');
         } else {
-            $(this).css("max-height", "0");
+            panel.style.maxHeight = '0';
         }
+
+        toggle.addEventListener('click', () => {
+
+            if (el.hasAttribute("opened")) {
+                panel.style.maxHeight = '0';
+                el.removeAttribute('opened');
+            } else {
+                panel.style.maxHeight = (panel.scrollHeight + 'px');
+                el.setAttribute('opened', '');
+            }
+
+        });
+
+        button.addEventListener('click', () => {
+            console.log(el.clientHeight);
+        });
+
     });
 
-    $("accordion-toggle").click(function () {
-        if ($(this).siblings("accordion-panel").height()) {
-            $(this).siblings("accordion-panel").css("max-height", "0");
-        } else {
-            $(this).siblings("accordion-panel").css("max-height", $(this).siblings("accordion-panel").prop("scrollHeight"));
-        }
-    });
-
-});
+};
