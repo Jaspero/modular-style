7df2a83cfb69a93455ac5fd2457414c617794e48
siclol <marko.milky@gmail.com>
2018-04-06 13:50:25 +0200

    Refactoring

--- assets/javascript/accordion.js
+++ assets/javascript/accordion.js
@@ -12,9 +12,6 @@ window.onload = () => {
 
         const accordionSummary = el.querySelector('.accordion_summary');
 
-        console.log('element:', el);
-        console.log('parent:', el.parentElement);
-
         if (el.classList.contains("active")) {
             el.style.maxHeight = (el.scrollHeight + 'px');
         } else {
@@ -37,17 +34,10 @@ window.onload = () => {
 };
 
 function setAccordionHeight(el, remove = false) {
-    let i = 0;
-
     while (el.parentElement) {
         if (el.parentElement.classList.contains('accordion')) {
-            console.log('has parent with classList accordion');
-            // el.parentElement.style.maxHeight = (parseInt(el.parentElement.style.maxHeight.replace('px', '')) + (remove ? -panel.scrollHeight : panel.scrollHeight)) + 'px';
+            el.parentElement.style.maxHeight = (parseInt(el.parentElement.style.maxHeight.replace('px', '')) + (remove ? -el.scrollHeight : el.scrollHeight)) + 'px';
         }
-
-        console.log(i, ' - element:', el, 'parent:', el.parentElement);
-        i++;
         el = el.parentElement;
     }
-
 }
